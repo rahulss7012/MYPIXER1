4f15a3999d60eec41391f62dfe097f989a87501f
rahulss7012 <108858605+rahulss7012@users.noreply.github.com>
2022-10-14 19:04:11 +0530

    Update site.py

--- site.py
+++ site.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.set_page_config(page_title="main page",page_icon="📷")
 st.title("main page")
 st.sidebar.success("select a page above")
+if "my_input" not in st.session_state:
+    st.session_state["my_input"]=""
+my_input=st.text_input("input a text here",st.session_state["my_input"])
+submit=st.button("submit")
+if submit:
+    st.session_state["my_input"]=my_input
+    st.write("you have entered",my_input)

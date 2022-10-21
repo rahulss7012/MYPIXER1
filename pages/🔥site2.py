6ac6f163d6614b5a27848de2d2c1730c41e1f947
KARTHIK-P-K <106388435+KARTHIK-P-K@users.noreply.github.com>
2022-10-21 14:04:18 +0530

    Update 🔥site2.py

--- pages/🔥site2.py
+++ pages/🔥site2.py
@@ -4,3 +4,19 @@ st.title("second page")
 st.subheader('blahh  blahhhh')
 st.write("HOLA AMIGOS")
 st.write("Danke Schon")
+
+
+import time
+
+st.title('st.progress')
+
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.balloons()
